f7ca76848c25ba649f0e4d8fd6d6e5ddbdaf45e6
Edgar Ribeiro <eneto774@gmail.com>
2024-10-11 02:22:47 -0300

    feat: create login page and integrated with oauth2 reddit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,68 @@
 // mark as client component
 'use client';
 
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 // importing necessary functions
 import { signIn, signOut, useSession } from 'next-auth/react';
+import Lottie from 'react-lottie';
+import reditAnimation from '../assets/lotties/redit-robot-logo.json';
+import { RedditIcon } from '../components/reddit-icon';
 
 export default function Home() {
-  // extracting data from usesession as session
   const { data: session } = useSession();
 
-  // checking if sessions exists
   if (session) {
-    // rendering components for logged in users
     return (
-      <div>
+      <div className="bg-zinc-950 dark:bg-white">
         <p>Welcome {session.user?.name}. Signed In As</p>
         <p>{session.user?.email}</p>
-        <button onClick={() => signOut()}>Sign out</button>
+        <Button onClick={() => signOut()}>Sign out</Button>
       </div>
     );
   }
 
+  const defaultLottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: reditAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   // rendering components for not logged in users
   return (
-    <div>
-      <p>Not Signed In</p>
-      <button onClick={() => signIn('reddit')}>Sign in with reddit</button>
+    <div className="bg-zinc-950 h-screen flex items-center justify-center">
+      <Card className="bg-zinc-950 w-[350px] h-[420px] text-foreground">
+        <CardHeader>
+          <Lottie options={defaultLottieOptions} height={200} width={200} />
+        </CardHeader>
+        <CardContent>
+          <CardTitle className="text-zinc-300">
+            Welcome to application.
+          </CardTitle>
+          <CardDescription>
+            For access is necessary sign in using Reddit.
+          </CardDescription>
+        </CardContent>
+        <CardFooter>
+          <Button
+            onClick={() => signIn('reddit')}
+            className="flex gap-3 font-medium w-full"
+          >
+            <RedditIcon size={20} />
+            Sign in with Reddit
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
